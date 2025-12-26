03e61bcb774966be4081283624ea263b729154a0
Abel <lewheabel16@gmail.com>
2025-12-26 23:26:14 +0100

    prisma error correct

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { UserController } from "#controllers/user.controller";
 import { asyncHandler } from "#lib/async-handler";
-import { auth } from "#middlewares/auth";
+// import { auth } from "#middlewares/auth";
 
 const router = Router();
 
 
 // Consultation de la liste ou d'un utilisateur
 router.get("/", asyncHandler(UserController.getAll));
-router.get("/:id",auth, asyncHandler(UserController.getById));
+router.get("/:id",asyncHandler(UserController.getById));
 
 export default router;
