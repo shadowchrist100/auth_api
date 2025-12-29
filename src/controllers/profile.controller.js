ff1d0034c8404048710eb1924c78ece5d2ef7e4c
OSSENI Rosmiyath <rosmiros52@gmail.com>
2025-12-29 14:53:40 +0100

    feat(profile): add profile controller with get/update/delete

---
+++ src/controllers/profile.controller.js
@@ -0,0 +1,52 @@
+import { ProfileService } from "#services/profile.service";
+import { UserDto } from "#dto/user.dto";
+export class ProfileController {
+	// GET /me
+	static async getMe(req, res) {
+		try {
+		const userId = req.user.id;
+		const user = await ProfileService.getMe(userId);
+		res.json({
+			success: true,
+			user: UserDto.transform(user),
+		});
+	} catch (error) {
+		res.status(500).json({ 
+			success: false,
+			 error: error.message,
+			});
+	}
+}
+// PUT /me
+	static async updateMe(req, res) {
+		try {
+		const userId = req.user.id;
+		const updatedUser = await ProfileService.updateMe(userId, req.body);
+		res.json({
+			success: true,
+			user: UserDto.transform(updatedUser),
+		});
+	} catch (error) {
+		res.status(error.status || 500).json({ 
+			success: false,
+			 error: error.message,
+			});
+	}
+}
+	// DELETE /me
+	static async deleteMe(req, res) {
+		try {
+		const userId = req.user.id;	
+		await ProfileService.deleteMe(userId);
+		res.json({
+			success: true,
+			message: "Compte désactivé avec succès",
+		});
+	} catch (error) {
+		res.status(error.status || 500).json({ 
+			success: false,
+			 error: error.message,
+			});
+	}
+}
+}
