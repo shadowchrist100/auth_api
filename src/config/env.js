7dda79d9b075aed04adeb4f1bb78c07d0de2be63
Abel <lewheabel16@gmail.com>
2025-12-30 17:31:45 +0100

    OAuth authentication with github

--- src/config/env.js
+++ src/config/env.js
@@ -7,9 +7,15 @@ export const config = {
   DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
 <<<<<<< HEAD
+<<<<<<< HEAD
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   GITHUB_STATE: process.env.GITHUB_STATE
 =======
 >>>>>>> 6764f0f (correction1)
+=======
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+  GITHUB_STATE: process.env.GITHUB_STATE
+>>>>>>> 6c18f1b (OAuth authentication with github)
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,6 +9,7 @@ router.post("/register", asyncHandler(UserController.register));
 router.post("/login", asyncHandler(UserController.login));
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 router.post("/refresh", asyncHandler(UserController.refresh));
 router.post("/logout", asyncHandler(UserController.logout));
 
@@ -16,10 +17,15 @@ router.post("/logout", asyncHandler(UserController.logout));
 router.post("/forgot_password", asyncHandler(UserController.forgotPassword));
 router.post("/reset_password", asyncHandler(UserController.resetPassword));
 
+=======
+>>>>>>> 6c18f1b (OAuth authentication with github)
 // authentification via github
 router.get("/auth/github", asyncHandler(UserController.githubAuth));
 router.get("/auth/githubCallback", asyncHandler(UserController.githubCallback));
 
+<<<<<<< HEAD
 =======
 >>>>>>> 6764f0f (correction1)
+=======
+>>>>>>> 6c18f1b (OAuth authentication with github)
 export default router;
