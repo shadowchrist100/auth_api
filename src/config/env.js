a61767aa39130cec68c2b42e5b42d3c46f48207b
Abel <lewheabel16@gmail.com>
2026-01-04 18:05:40 +0100

    resolving user.controller conflict

--- src/config/env.js
+++ src/config/env.js
@@ -6,16 +6,7 @@ export const config = {
   NODE_ENV: process.env.NODE_ENV,
   DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
-<<<<<<< HEAD
-<<<<<<< HEAD
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   GITHUB_STATE: process.env.GITHUB_STATE
-=======
->>>>>>> 6764f0f (correction1)
-=======
-  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-  GITHUB_STATE: process.env.GITHUB_STATE
->>>>>>> 6c18f1b (OAuth authentication with github)
 };
